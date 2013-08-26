ce0de6f9596aad43412ffc0e1f502cc0d3d8944a
voutasaurus <voutasaurus@gmail.com>
2013-08-27 07:37:27 +1000

    Fixed linked list Memory problem fixed by allocating the memory at the appropriate function level.

--- DataStructuresReview/linkedList.c
+++ DataStructuresReview/linkedList.c
@@ -8,8 +8,8 @@
 
 struct linkedListEntry *newLL(void);
 struct linkedListEntry *newLLwithvalue(int value);
-struct linkedListEntry *consLL (struct linkedListEntry *front, int valueToInsert);
-struct linkedListEntry *insertLL (struct linkedListEntry *front, int valueToInsert);
+struct linkedListEntry *consLL (struct linkedListEntry *front, struct linkedListEntry *newEntry, int valueToInsert);
+struct linkedListEntry *insertLL (struct linkedListEntry *front, struct linkedListEntry *newEntry, int valueToInsert);
 void printLL (struct linkedListEntry *front);
 
 // Data Structure for Linked List
@@ -21,6 +21,10 @@ struct linkedListEntry
 
 
 // Function to create an empty list
+// What does this do since the memory allocated for this struct is
+// deallocated when this function returns?
+// It's supposed to be a null pointer, and it seems to work anyway
+// probably best to check this somehow
 struct linkedListEntry *newLL (void)
 {
 	return ((struct linkedListEntry *) 0);
@@ -33,38 +37,33 @@ struct linkedListEntry *newLL (void)
 	listptr = newLL();
 	listptr = &(consLL(listptr, value));
 
-*/
-struct linkedListEntry *newLLwithvalue (int val)
+// THIS is difficult because the memory has to be allocated at the level it is being used
+// The list pointer and the new entry pointer both have to be passed, and by that time
+// you may as well do it the other way
+// Repeat - Memory allocated within a function is fair game for the rest of the system when the function returns
+struct linkedListEntry *newLLwithvalue (struct linkedListEntry *space_ptr, int val)
 {
-	struct linkedListEntry *listptr;
-	listptr = newLL();
-	listptr = consLL(listptr, val);
+	space_ptr = newLL();
+	space_ptr = consLL(space_ptr, , val);
 
 	return listptr;
 }
+*/
 
 // Function to add a value to the front of a list 
-struct linkedListEntry *consLL (struct linkedListEntry *front, int valueToInsert)
+struct linkedListEntry *consLL (struct linkedListEntry *front, struct linkedListEntry *newEntry, int valueToInsert)
 {
+	newEntry->value = valueToInsert; // The value of the entry from input
+	newEntry->next = front; // This new entry points to the old front of the list
 
-	struct linkedListEntry newEntry; // The entry we want to insert (allocates memory)
-	struct linkedListEntry *newFront = &newEntry; // Pointer to the entry
-
-	newFront->value = valueToInsert; // The value of the entry from input
-	newFront->next = front; // This new entry points to the old front of the list
-
-	return newFront; // Return the new front of the list
+	return newEntry; // Return the new front of the list
 	// Note that when this function is called, it should be newlist = &(consLL(listptr, val));
 	// to get the address of the new list
-
 }
 
 // Function to insert a value into a sorted linked list
-struct linkedListEntry *insertLL (struct linkedListEntry *front, int valueToInsert)
+struct linkedListEntry *insertLL (struct linkedListEntry *front, struct linkedListEntry *newEntry, int valueToInsert)
 {
-
-	struct linkedListEntry newEntrySpace;	// The entry we want to insert (allocates memory)
-	struct linkedListEntry *newEntry = &newEntrySpace; // Pointer to the new entry
 	newEntry->value = valueToInsert; // The value of that entry from input
 
 	struct linkedListEntry *current = front; // To cycle through the list
@@ -100,7 +99,6 @@ struct linkedListEntry *insertLL (struct linkedListEntry *front, int valueToInse
 
 	// Note that when this function is called, it should be newlist = &(insertLL(listptr, val));
 	// to get the new address of the new list
-
 }
 
 // Function to print the contents of a list
@@ -122,33 +120,9 @@ void testLL (void)
 
 	// NONSORTED
 
-	// Initialise
-	struct linkedListEntry *listptr = newLL();
-	struct linkedListEntry *listptra;
-	struct linkedListEntry *listptrb;
-
-	printLL(listptr);
-
-	printf("BREAK \n");
-
-	// Add Data
-	listptra = consLL(listptr, 1);
-	listptrb = consLL(listptra, 2);
-//	for (i = 0; i <= 10; i++)
-//		listptr = consLL(listptr, 10 - i);
-
-	// Print
-	printf("listptr: ");
-	printLL(listptr);
-	printf("\n listptr: ");
-	printLL(listptra);
-	printf("\n listptrb: ");
-	printLL(listptrb);
-
-	printf("BREAK \n");
-
+	// Test 1: manual list construction
 	struct linkedListEntry n1, n2, n3;
-	struct linkedListEntry *list_pointer = &n1;
+	struct linkedListEntry *list_pointer_manual = &n1;
 
 	n1.value = 100;
 	n1.next = &n2;
@@ -157,20 +131,108 @@ void testLL (void)
 	n3.value = 300;
 	n3.next = (struct linkedListEntry *) 0; // end of the list - null pointer
 
-	printLL(list_pointer);
+	printf("Manual list print: \n");
+	printf("==================== \n");
+	printf("Expected: \n");
+	printf("==================== \n");
+	printf("100 \n");
+	printf("200 \n");
+	printf("300 \n");
+	printf("==================== \n");
+	printf("Actual: \n");
+	printf("==================== \n");
+	printLL(list_pointer_manual);
+	printf("==================== \n");
 
 	// null test
-	struct linkedListEntry *list_pointer_thing = (struct linkedListEntry *) 0;
+	struct linkedListEntry *list_pointer_nullTest = (struct linkedListEntry *) 0;
 
-	printf("list_pointer_thing: ");
-	printLL(list_pointer_thing);
+	printf("Null list print (Manual version): \n");
+	printf("==================== \n");
+	printf("Expected: \n");
+	printf("==================== \n");
+	printf("==================== \n");
+	printf("Actual: \n");
+	printf("==================== \n");
+	printLL(list_pointer_nullTest);
+	printf("==================== \n");
 
-	// Add More Data
-//	for (i = 0; i <= 10; i++)
-//		listptr = consLL(listptr, i);
+	// Test 2: newLL()
 
-	// Print again
-//	printLL(listptr);
+	struct linkedListEntry *list_pointer_newLL = newLL();
+
+	printf("Null list print (newLL version): \n");
+	printf("==================== \n");
+	printf("Expected: \n");
+	printf("==================== \n");
+	printf("==================== \n");
+	printf("Actual: \n");
+	printf("==================== \n");
+	printLL(list_pointer_newLL);
+	printf("==================== \n");
+
+
+	// Test 3: consLL()
+
+	struct linkedListEntry *list_pointer_consLL = newLL();
+	
+	// Add to new list
+	// Must allocate space for the new entry at the level that the list is being used
+	// Otherwise the memory will be collected and everything will be stuffed
+	struct linkedListEntry newEntrySpace; 
+	struct linkedListEntry *newEntry = &newEntrySpace;
+
+	list_pointer_consLL = consLL(list_pointer_consLL, newEntry, 2);
+
+	printf("Cons list print (consLL): \n");
+	printf("==================== \n");
+	printf("Expected: \n");
+	printf("==================== \n");
+	printf("2 \n");
+	printf("==================== \n");
+	printf("Actual: \n");
+	printf("==================== \n");
+	printLL(list_pointer_consLL);
+	printf("==================== \n");
+
+	struct linkedListEntry *list_pointer_consLL_mult = newLL();
+
+	struct linkedListEntry newEntrySpaces[10]; 
+	struct linkedListEntry *newEntrySpot = &(newEntrySpaces[0]);
+
+	list_pointer_consLL_mult = consLL(list_pointer_consLL_mult, newEntrySpot, 0);
+	
+	for (i = 1; i < 10; i++)
+	{
+		newEntrySpot = &(newEntrySpaces[i]);
+		list_pointer_consLL_mult = consLL(list_pointer_consLL_mult, newEntrySpot, i);
+	}
+
+	printf("Cons mult list print (consLL): \n");
+	printf("==================== \n");
+	printf("Expected: \n");
+	printf("==================== \n");
+	printf("9 \n");
+	printf("8 \n");
+	printf("7 \n");
+	printf("6 \n");
+	printf("5 \n");
+	printf("4 \n");
+	printf("3 \n");
+	printf("2 \n");
+	printf("1 \n");
+	printf("0 \n");
+	printf("==================== \n");
+	printf("Actual: \n");
+	printf("==================== \n");
+	printLL(list_pointer_consLL_mult);
+	printf("==================== \n");
+
+	
+	// Test 4: insertLL
+
+	struct linkedListEntry *list_pointer_insertLL = newLL();
+
 
 	// Remove Data (not implemented)
 	// Print
